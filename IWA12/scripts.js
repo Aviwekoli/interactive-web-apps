81edc838be4b0947e94cd908e8a0c6086a29c1dd
Aviwekoli <aviwekoli@gmail.com>
2023-08-23 11:53:29 +0200

    Added IWA14

--- IWA12/scripts.js
+++ IWA12/scripts.js
@@ -26,9 +26,13 @@ const STATUS_MAP = {
 }
 
 // Edit below line 
+ const buttonElem = document.querySelectorAll('button');
+ for (i =0; i< buttonElem.length; i++) {
+     buttonElem[i].style.color = '';
+ }
 /**
  * Style edits for Book 2 (THE GOOD BOOK) using the DOM manipulation
- * Here I used the STATUS_MAP Object to make the necessary changes such as
+ * the STATUS_MAP Object is used to make the necessary changes such as
  * changing the color of buttons and disabling certain buttons that need to be disabled
  */
 const book1 = document.querySelector('#book1');
@@ -43,7 +47,7 @@ const checkout0 = book1.querySelector('.checkout');
 checkout0.disabled = !(STATUS_MAP.overdue.canCheckout);
 
 const checkin0 = book1.querySelector('.checkin');
-checkin0.style.color = '';
+//checkin0.style.color = '';
 checkin0.disabled = !(STATUS_MAP.overdue.canCheckIn);
 
 /**
@@ -63,7 +67,7 @@ let checkout2 = book2.querySelector('.checkout');
 checkout2.disabled = !(STATUS_MAP.reserved.canCheckout);
 
 let checkin2 = book2.querySelector('.checkin');
-checkin2.style.color = '';
+//checkin2.style.color = '';
 checkin2.disabled = !(STATUS_MAP.reserved.canCheckIn);
 
 /**
@@ -83,7 +87,7 @@ const checkout3 = book3.querySelector('.checkout');
 checkout3.disabled = !(STATUS_MAP.shelf.canCheckout);
 
 const checkin3 = book3.querySelector('.checkin');
-checkin3.style.color = '';
+//checkin3.style.color = '';
 checkin3.disabled = !(STATUS_MAP.shelf.canCheckIn);
 
 
