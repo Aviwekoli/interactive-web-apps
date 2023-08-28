070a0e782e56c090ee995c15728d1d250e54699a
Aviwekoli <aviwekoli@gmail.com>
2023-08-28 23:41:59 +0200

    Added the following handlers: handleAddToggle, handleHelpToggle and handleAddSubmit

--- IWA18/scripts.js
+++ IWA18/scripts.js
@@ -1,4 +1,23 @@
 /**
+ * Importing functions from view.js and data.js 
+ */
+import {html} from './view.js';
+import {createOrderHtml} from './view.js';
+//import {createTableOptionsHtml} from './view.js';
+import {updateDraggingHtml} from './view.js';
+import {moveToColumn} from './view.js';
+import {createOrderData} from './data.js';
+//import {createUniqueId} from './data.js';
+//const {help: {overlay, cancel}} = html;
+// const overlap1 = document.querySelector('[data-help-overlay]');
+// const help1 = document.querySelector('[data-help]');
+
+// console.log(overlap1);
+
+// help1.addEventListener('click', function(){
+//     overlap1.style.display = 'block';
+// })
+/**
  * A handler that fires when a user drags over any element inside a column. In
  * order to determine which column the user is dragging over the entire event
  * bubble path is checked with `event.path` (or `event.composedPath()` for
@@ -28,14 +47,48 @@ const handleDragOver = (event) => {
 }
 
 
-const handleDragStart = (event) => {}
-const handleDragEnd = (event) => {}
-const handleHelpToggle = (event) => {
-    
+const handleDragStart = (event) => {
+
 }
-const handleAddToggle = (event) => {}
-const handleAddSubmit = (event) => {}
-const handleEditToggle = (event) => {}
+const handleDragEnd = (event) => {}
+
+const handleHelpToggle = (event) => {
+    const {help: {overlay, cancel}} = html;
+    overlay.style.display = 'block';
+    cancel.addEventListener('click', function(){
+        overlay.style.display = 'none';
+    })   
+}
+
+const handleAddToggle = (event) => {
+    const {add: {overlay, form, cancel, title, table}} = html;
+    const {other: add} = html;
+    const {help: {overlay: overlay1}} = html;
+    const addButton = document.querySelector('.button_primary');
+    overlay.style.display = 'block';
+    cancel.addEventListener('click', function(){
+        overlay.style.display = 'none';
+        title.value = '';
+        table.value = '';
+    })
+}
+
+ const handleAddSubmit = (event) => {
+    event.preventDefault();
+    const {columns:{ordered},
+           add: {overlay, form, title, table}} = html;
+    const newOrder = createOrderHtml(createOrderData({
+        title: title.value,
+        table: table.value,
+        column: 'ordered',
+    }));
+    ordered.appendChild(newOrder);
+    form.reset();
+    overlay.style.display = '';
+}
+const handleEditToggle = (event) => {
+    const { edit: overlay, title, table, id, column } = html;
+}
 const handleEditSubmit = (event) => {}
 const handleDelete = (event) => {}
 
